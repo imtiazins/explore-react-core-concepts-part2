8d6b01ed7a6708689f2bb811ab04edff2550309e
“imtiazins” <imtiaz3996@gmail.com>
2023-09-18 01:04:12 +0600

    different ways to add event handle event in react

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,22 @@ import './App.css'
 
 function App() {
  
+  function handleClick(){
+    alert('button clicked');
+  }
+  const handleClick2=()=>{
+    alert('btn clicked on 2');
+  }
+
+  const addToFive =(num)=>{
+    alert(num +5);
+  }
   return (
     <>
       <h1>React Core Concepts 2</h1> 
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={handleClick2}>Clicked-2</button>
+      <button onClick={()=>addToFive(3)}>Four </button>
     </>
   )
 }
