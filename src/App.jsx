c302bc86379e87ad0e1d82af94f85ebf890b9356
“imtiazins” <imtiaz3996@gmail.com>
2023-09-18 00:28:33 +0600

    creat for exploring part 2 of core concepts of react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <h1>React Core Concepts 2</h1> 
+    </>
+  )
+}
+
+export default App
